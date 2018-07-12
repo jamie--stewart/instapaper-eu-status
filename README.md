83d9ce57b5e5d58dd6c44ff25ac76bfd302bc54f
Jamie Stewart <jamie.stewart5@gmail.com>
2018-07-12 20:43:33 +0100

    Add 1.0 of checker

---
+++ README.md
@@ -0,0 +1,19 @@
+#Instapaper Status 
+
+> Check if Instapaper is _still_ down, and report this to Pushover 
+
+I'm getting sick of checking if Instapaper is back online for us in the EU. This simple utility will tell me every day if I can use it again. Bets on how big this number gets?
+
+## Usage
+
+* Clone the project
+* `pipenv install`
+* `pipenv shell`
+* `python instapaper_checker.py`
+	* expects a [python-pushover config file](https://github.com/Thibauth/python-pushover#configuration) at `./pushoverrc`
+* Schedule it however you like 👌
+
+Uses [Requests](https://github.com/requests/requests), [Arrow](https://github.com/crsmithdev/arrow) and [python-pushover](https://github.com/Thibauth/python-pushover)
+
+## Output 
+![Pushover Notification](./pushover_output.png)
---
+++ instapaper_checker.py
@@ -0,0 +1,35 @@
+import requests
+import arrow
+from dateutil import tz
+from pushover import Client
+
+GDPR_MESSAGE = 'Instapaper is temporarily unavailable for residents in Europe'
+
+def send_pushover_notification(message):
+    print ('Sending Pushover notification')
+    Client(config_path='./pushoverrc').send_message(message, title="Instapaper status:")
+
+def instapaper_page_has_gdpr_message():
+    request = requests.get('https://www.instapaper.com/')
+    return GDPR_MESSAGE in request.text
+
+def days_between_now_and_gdpr_day():
+    gdpr_day = arrow.get(2018, 5, 25, tzinfo=tz.gettz('Europe/London'))
+    return (arrow.utcnow().to('Europe/London') - gdpr_day).days
+    
+
+def check_instapaper_status():
+
+    print('{}: Checking Instapaper EU status'.format(arrow.utcnow().to('Europe/London').format('YYYY-MM-DD HH:mm:ss')))
+    days_since_gdpr = days_between_now_and_gdpr_day()
+    
+    if (instapaper_page_has_gdpr_message()):
+        message = 'Instapaper is STILL is not up for EU users. GDPR came in {} days ago...'.format(days_since_gdpr)
+    else:
+        message = 'Instapaper is back up! After a mere {} days...'.format()
+
+    send_pushover_notification(message)
+
+if __name__ == '__main__':
+    check_instapaper_status()
+    print('\n')
